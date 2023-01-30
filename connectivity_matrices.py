e93f454da4fe7f9ef1ac2d69e62b5b304d760923
Dom <dominicd7@hotmail.com>
2023-01-30 14:54:47 +0100

    Add locust network to analysis

--- connectivity_matrices.py
+++ connectivity_matrices.py
@@ -1,36 +1,76 @@
+import itertools
+import os
+from typing import Callable
 import numpy as np
 import scipy.io
 
+# Note: Generate all the functions by passing these
+
+class ConnectivityMatrix():
+	def __init__(self, num_pen: int, num_peg: int, num_epg: int, num_d7: int, loader: Callable[[], np.ndarray]) -> None:
+		self.NUM_PEN = num_pen
+		self.NUM_PEG = num_peg
+		self.NUM_EPG = num_epg
+		self.NUM_D7 = num_d7
+		self.CONNECTIVITY_MATRIX_SIZE = self.NUM_PEN + self.NUM_PEG + self.NUM_EPG + self.NUM_D7
+
+		self.SLICE_PEN = slice(0, self.NUM_PEN)
+		self.SLICE_PEG = slice(self.NUM_PEN,self.NUM_PEN+self.NUM_PEG)
+		self.SLICE_EPG = slice(self.NUM_PEN+self.NUM_PEG,self.NUM_PEN+self.NUM_PEG+self.NUM_EPG)
+		self.SLICE_D7 = slice(self.NUM_PEN+self.NUM_PEG+self.NUM_EPG,self.NUM_PEN+self.NUM_PEG+self.NUM_EPG+self.NUM_D7)
+
+		self.GET_MATRIX = loader
+
+slice_to_range = lambda slice: range(slice.start if slice.start else 0, slice.stop, slice.step if slice.step else 1)
+
 def get_fly_connectivity_matrix_raw():
-	return scipy.io.loadmat('Insect Head Direction Network/connectivity_matrix_drosophila_mine_case_5_9cols_labels1.mat')['con_matrix'].T
+	return scipy.io.loadmat(os.path.join(os.path.dirname(__file__), 'connectivity_matrix_drosophila_mine_case_5_9cols_labels1.mat'))['con_matrix'].T
 
-def get_fly_connectivity_matrix_bool():
-	return get_fly_connectivity_matrix_raw() != 0
+def get_locust_connectivity_matrix_raw():
+	return scipy.io.loadmat(os.path.join(os.path.dirname(__file__), 'connectivity_matrix_locust_mine_case_6_with3s_labels_1.mat'))['con_matrix'].T
 
-def get_fly_connectivity_matrix_string():
-	connectivity_matrix = get_fly_connectivity_matrix_raw()
+FLY = ConnectivityMatrix(16, 18, 18, 8, get_fly_connectivity_matrix_raw)
+LOCUST = ConnectivityMatrix(16, 16, 16, 8, get_locust_connectivity_matrix_raw)
 
-	PEN = slice(16)
-	PEG = slice(16,16+18)
-	EPG = slice(16+18,16+18+18)
-	D7 = slice(16+18+18,16+18+18+8)
-	connections_PEN_EPG = np.nonzero(connectivity_matrix[PEN,EPG])
-	connections_PEG_EPG = np.nonzero(connectivity_matrix[PEG,EPG])
-	connections_EPG_PEN = np.nonzero(connectivity_matrix[EPG,PEN])
-	connections_EPG_PEG = np.nonzero(connectivity_matrix[EPG,PEG])
-	connections_EPG_D7 = np.nonzero(connectivity_matrix[EPG,D7])
-	connections_D7_PEN = np.nonzero(connectivity_matrix[D7,PEN])
-	connections_D7_PEG = np.nonzero(connectivity_matrix[D7,PEG])
-	connections_D7_D7 = np.nonzero(connectivity_matrix[D7,D7])
+def get_connectivity_matrix_bool(CM: ConnectivityMatrix):
+	return CM.GET_MATRIX() != 0
 
+get_fly_connectivity_matrix_bool = lambda : get_connectivity_matrix_bool(FLY)
+get_locust_connectivity_matrix_bool = lambda : get_connectivity_matrix_bool(LOCUST)
+
+def get_connectivity_matrix_string(CM: ConnectivityMatrix):
+	'''Get the connectivity matrix with each cell either 0 (no connection) or `'connections_{pre_name}_{post_name}'` if the neurons are connected.'''
+	connectivity_matrix = CM.GET_MATRIX()
 	conn_mat_str = np.zeros_like(connectivity_matrix, dtype=object)
-	conn_mat_str[PEN,EPG][connections_PEN_EPG] = 'connections_PEN_EPG'
-	conn_mat_str[PEG,EPG][connections_PEG_EPG] = 'connections_PEG_EPG'
-	conn_mat_str[EPG,PEN][connections_EPG_PEN] = 'connections_EPG_PEN'
-	conn_mat_str[EPG,PEG][connections_EPG_PEG] = 'connections_EPG_PEG'
-	conn_mat_str[EPG,D7][connections_EPG_D7] = 'connections_EPG_D7'
-	conn_mat_str[D7,PEN][connections_D7_PEN] = 'connections_D7_PEN'
-	conn_mat_str[D7,PEG][connections_D7_PEG] = 'connections_D7_PEG'
-	conn_mat_str[D7,D7][connections_D7_D7] = 'connections_D7_D7'
 
-	return connectivity_matrix
+	for (pre_slice, pre_name), (post_slice, post_name) in itertools.product(zip([CM.SLICE_PEN, CM.SLICE_PEG, CM.SLICE_EPG, CM.SLICE_D7], ['PEN', 'PEG', 'EPG', 'D7']), repeat=2):
+		conn_mat_str[pre_slice,post_slice][np.nonzero(connectivity_matrix[pre_slice,post_slice])] = f'connections_{pre_name}_{post_name}'
+
+	return conn_mat_str
+
+get_fly_connectivity_matrix_string = lambda : get_connectivity_matrix_string(FLY)
+get_locust_connectivity_matrix_string = lambda : get_connectivity_matrix_string(LOCUST)
+
+def neuron_index_to_name(CM: ConnectivityMatrix, neuron_index: int):
+	assert neuron_index >= 0 and neuron_index < CM.CONNECTIVITY_MATRIX_SIZE, f'Neuron index {neuron_index} out of range of connectivity matrix'
+
+	for index_range, name in zip([slice_to_range(slice) for slice in [CM.SLICE_PEN, CM.SLICE_PEG, CM.SLICE_EPG, CM.SLICE_D7]], ['PEN', 'PEG', 'EPG', 'D7']):
+		if neuron_index in index_range:
+			return f'{name}_{index_range.index(neuron_index)}'
+	
+	raise IndexError(f"Can't find index {neuron_index} in connectivity matrix.")
+
+fly_neuron_index_to_name = lambda neuron_index : neuron_index_to_name(FLY, neuron_index)
+locust_neuron_index_to_name = lambda neuron_index : neuron_index_to_name(LOCUST, neuron_index)
+
+def get_connectivity_matrix_edge_list(CM: ConnectivityMatrix):
+	connectivity_matrix = CM.GET_MATRIX()
+	index_to_name = lambda neuron_index : neuron_index_to_name(CM, neuron_index)
+
+	return [(index_to_name(pre), index_to_name(post), {'weight': 5 if index_to_name(pre).split('_')[0]=='D7' else 1}) \
+		for pre in range(CM.CONNECTIVITY_MATRIX_SIZE) \
+		for post in range(CM.CONNECTIVITY_MATRIX_SIZE) \
+		if connectivity_matrix[pre,post]]
+
+get_fly_connectivity_matrix_edge_list = lambda : get_connectivity_matrix_edge_list(FLY)
+get_locust_connectivity_matrix_edge_list = lambda : get_connectivity_matrix_edge_list(LOCUST)
